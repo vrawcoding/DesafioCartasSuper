24f491e068ccc7d9d70182d96a207cd093482411
vrawcoding <gbrielolvr20@gmail.com>
2025-09-07 01:44:35 +0000

    alguns erros corrigidos

--- super_trunfo.c
+++ super_trunfo.c
@@ -2,20 +2,20 @@
 
 int main(){
     // declaraçao de variaveis da primeira carta
-    char estado1, codcarta1[10], cidade1[20];
+    char estado1[20], codcarta1[10], cidade1[20];
     int populacao1, pontosT1;
     float area1, pib1;
 
     // declaraçao de variaveis da segunda carta
-    char estado2, codcarta2[10], cidade2[20];
+    char estado2[20], codcarta2[10], cidade2[20];
     int populacao2, pontosT2;
     float area2, pib2;
 
-    printf ("======= CARTA 1 =======\n");
+    printf ("\n======= CARTA 1 =======\n");
 
     // area de cadastramento da primeira carta
-    printf("Digite O Estado (A..H): ");
-    scanf("%s", codcarta1);
+    printf("Digite O Estado: ");
+    scanf("%s", estado1);
 
     printf("Digite O Código Da Cidade (ex: B02): ");
     scanf("%s", codcarta1);
@@ -35,11 +35,11 @@ int main(){
     printf("Quantos Pontos Turísticos Possui?: ");
     scanf("%f", &pontosT1);
 
-    printf ("======= CARTA 2 =======\n");
+    printf ("\n======= CARTA 2 =======\n");
 
     // area de cadastramento da segunda carta
-    printf("Digite O Estado (A..H): ");
-    scanf("%s", codcarta2);
+    printf("Digite O Estado: ");
+    scanf("%s", estado2);
 
     printf("Digite O Código Da Cidade (ex: B02): ");
     scanf("%s", codcarta2);
@@ -60,27 +60,28 @@ int main(){
     scanf("%f", &pontosT2);
 
 
-    printf("         Carta 1         \n");
+    printf("\n        Carta 1         \n");
 
     //area de exibiçao da primeira carta
-    printf("Estado: %s\n", codcarta1);
+    printf("Estado: %s\n", estado1);
     printf("Código Da Carta: %s\n", codcarta1);
-    printf("Cidade: %s\n");
+    printf("Cidade: %s\n", cidade1);
     printf("Populaçao: %d\n", populacao1);
-    printf("Àrea: %f\n", area1);
-    printf("PIB: %f\n", pib1);
+    printf("Àrea: %.2f\n Km²", area1);
+    printf("PIB: %.2f\n", pib1);
     printf("Pontos Turísticos: %d\n", pontosT1);
 
-     printf("         Carta 1        \n");
+    printf("\n         Carta 2      \n");
 
     //area de exibiçao da segunda carta
-    printf("Estado: %s\n", codcarta1);
-    printf("Código Da Carta: %s\n", codcarta1);
-    printf("Cidade: %s\n");
-    printf("Populaçao: %d\n", populacao1);
-    printf("Àrea: %f\n", area1);
-    printf("PIB: %f\n", pib1);
-    printf("Pontos Turísticos: %d\n", pontosT1);
+    printf("Estado: %s\n", estado2);
+    printf("Código Da Carta: %s\n", codcarta2);
+    printf("Cidade: %s\n", cidade2);
+    printf("Populaçao: %d\n", populacao2);
+    printf("Àrea: %.2f\n Km²", area2);
+    printf("PIB: %.2f\n", pib2);
+    printf("Pontos Turísticos: %d\n", pontosT2);
+
 
     return 0;
  }
